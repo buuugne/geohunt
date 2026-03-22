cb3637dac68ab9cfdf9cafa2745e00c6d37fc55f
Izabela <izabelaval6@gmail.com>
2026-03-23 01:51:20 +0200

    feat: add UpdatePlayerRankingAsync to LeaderboardService

--- psi25-project/Services/Interfaces/ILeaderboardService.cs
+++ psi25-project/Services/Interfaces/ILeaderboardService.cs
@@ -1,10 +1,14 @@
 using psi25_project.Models.Dtos;
-
+ 
 namespace psi25_project.Services.Interfaces
 {
     public interface ILeaderboardService
     {
         Task<List<LeaderboardEntry>> GetTopLeaderboardAsync(int top = 20);
         Task<List<LeaderboardEntry>> GetTopPlayersAsync(int top = 20);
+ 
+        // Called by GameService when a game finishes
+        Task UpdatePlayerRankingAsync(Guid userId, int gameScore);
     }
 }
+ 
